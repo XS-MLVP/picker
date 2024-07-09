14bee1bc97aeb5f5c067ab9875757d86c7e906a3
yaozhicheng <yaozhicheng@ict.ac.cn>
2024-07-09 18:16:51 +0800

    ignore output

--- template/lib/mk/python.mk
+++ template/lib/mk/python.mk
@@ -7,9 +7,9 @@ endif
 	@cp python/Makefile ${TARGET}/Makefile
 	@cp python/dut.i ${TARGET}/dut.i
 	@cp python/dut.py ${TARGET}/__init__.py
-	cd ${TARGET} && make && make clean
+	@cd ${TARGET} && make && make clean
 
 ifeq ($(VERBOSE), OFF)
-	cd ${TARGET} && rm -rf CMakeLists.txt dut* *.hpp build Makefile __pycache__ *.cmake
+	@cd ${TARGET} && rm -rf CMakeLists.txt dut* *.hpp build Makefile __pycache__ *.cmake
 endif
 
