14790c4a320d2a21f272b9eb40f89b5b8fe8a839
yaozhicheng <yaozhicheng@ict.ac.cn>
2024-07-12 16:09:01 +0800

    uniform mk

--- template/java/example.java
+++ template/java/example.java
@@ -4,7 +4,7 @@ public class example {
 	public static void main(String[] args) {
 	  UT_{{__TOP_MODULE_NAME__}} dut = new UT_{{__TOP_MODULE_NAME__}}();
 	  for(int i = 0; i<10; i++){
-		dut.step();
+		dut.Step();
 	  }
 	  dut.Finish();
 	  System.out.println("Step {{__TOP_MODULE_NAME__}} 10 times complete");
--- template/lib/mk/golang.mk
+++ template/lib/mk/golang.mk
@@ -1,16 +1,9 @@
 default:
-ifeq ($(EXAMPLE), ON)
 	@cp golang/example.go ${TARGET}/../example.go
-endif
 	@cp golang/cmake/${SIMULATOR}.cmake ${TARGET}/dut.cmake
 	@cp golang/CMakeLists.txt ${TARGET}/CMakeLists.txt
 	@cp golang/Makefile ${TARGET}/Makefile
 	@cp golang/dut.i ${TARGET}/dut.i
 	@cp golang/dut.go ${TARGET}/${TARGET}_Wrapper.go
 	@cp golang/go.mod ${TARGET}/go.mod
-	@cd ${TARGET} && make && make clean
-
-ifeq ($(VERBOSE), OFF)
-	@cd ${TARGET} && rm -rf CMakeLists.txt dut* *.hpp build Makefile __pycache__ *.cmake
-endif
-
+	@cd ${TARGET} && make
