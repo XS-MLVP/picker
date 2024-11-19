9cd96a112276ca204e37eba2928d95780b57bcbc
yaozhicheng <yaozhicheng@ict.ac.cn>
2024-11-19 12:23:18 +0800

    exmaple: fix Cache --tdir

--- example/Cache/release-verilator.sh
+++ example/Cache/release-verilator.sh
@@ -14,8 +14,8 @@ fi
 
 # run cache codegen
 rm -rf picker_out
-./build/bin/picker export --autobuild false example/Cache/Cache.v --fs example/Cache/Test.v -w cache.vcd --sim verilator $@
+./build/bin/picker export --autobuild false example/Cache/Cache.v --fs example/Cache/Test.v -w cache.vcd --sim verilator --tdir ./picker_out/ $@
 
 # build cache
-cd picker_out && make
+cd picker_out/Cache && make
 # echo "build cache done, all you need has been generated in temp/release"
