a8697aae82fad5cff04e81401750a8b835201f2b
Makiras <22570332+Makiras@users.noreply.github.com>
2025-01-02 15:35:17 +0800

    fix: VCS compiler options for vpi force

--- template/lib/cmake/vcs.cmake
+++ template/lib/cmake/vcs.cmake
@@ -30,12 +30,16 @@ if(SIMULATOR STREQUAL "vcs")
 	file(COPY ${SOURCES} DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
 
 	# CFLAGS Detect IF CFLAGS is not empty, add -cflags to VCS compile
-	separate_arguments(CFLAGS)
 	set(SIMULATOR_CFLAGS "")
 	if(NOT "${CFLAGS}" STREQUAL "")
 		# add -CFLAGS to VCS compile
-		set(SIMULATOR_CFLAGS "-CFLAGS" "${CFLAGS}")
+		set(SIMULATOR_CFLAGS "-CFLAGS ${CFLAGS}")
 		message(STATUS "VCS CFLAGS: ${CFLAGS}")
+	endif()
+	
+	# if vpi is enabled, add SIMULATOR_FLAGS '+vpi' and '-debug_access+all'
+	if(${VPI} STREQUAL "ON")
+		set(SIMULATOR_FLAGS "${SIMULATOR_FLAGS} +vpi -debug_access+all")
 	endif()
 
 	# Using Compiled vcs dynamic library
@@ -52,10 +56,11 @@ if(SIMULATOR STREQUAL "vcs")
 			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 			COMMAND
 				vcs -e VcsMain -slave ${VCS_TRACE} -sverilog -lca -l compile.log
-				-top ${ModuleName}_top
-				-full64 +vpi -timescale=1ns/1ps ${ModuleName}_top.sv ${ModuleName}.v -f
-				filelist.f -o libDPI${ModuleName}.so +modelsave -LDFLAGS "-shared"
-				${SIMULATOR_FLAGS} ${SIMULATOR_CFLAGS}
+				-top ${ModuleName}_top -full64 -timescale=1ns/1ps 
+				${ModuleName}_top.sv ${ModuleName}.v -f filelist.f 
+				-o libDPI${ModuleName}.so +modelsave -LDFLAGS "-shared"
+				${SIMULATOR_FLAGS} 
+				${SIMULATOR_CFLAGS}
 				-P ${VERDI_HOME}/share/PLI/VCS/LINUX64/novas.tab
 				${VERDI_HOME}/share/PLI/VCS/LINUX64/pli.a)
 	endif()
