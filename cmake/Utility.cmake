2d08c7bdc7032e6fcc814419d39a73bfc225da80
Makiras <22570332+Makiras@users.noreply.github.com>
2025-02-28 18:01:04 +0800

    feat: add date info in picker version

--- cmake/Utility.cmake
+++ cmake/Utility.cmake
@@ -3,7 +3,7 @@ macro(get_git_hash _git_hash)
 	find_package(Git QUIET)
 	if(GIT_FOUND)
 		execute_process(
-			COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
+			COMMAND ${GIT_EXECUTABLE} log -1 --date=short --pretty=format:"%h-%cd"
 			OUTPUT_VARIABLE ${_git_hash}
 			OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET
 			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
