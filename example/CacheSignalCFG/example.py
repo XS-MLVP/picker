3af2c8c32dad16e91e0455a3fafdc60e47ff5e22
yaozhicheng <yaozhicheng@ict.ac.cn>
2025-03-11 13:37:01 +0800

    python: refine typo

--- example/CacheSignalCFG/example.py
+++ example/CacheSignalCFG/example.py
@@ -10,27 +10,21 @@ except:
 if __name__ == "__main__":
     dut = DUTCacheCFG()
     dut.InitClock("clock")
-
-    print(dut.xcfg.GetSignalNames("CacheCFG_top.clock"))
-
-    clk = dut.xcfg.NewXData("CacheCFG_top.clock")
-    rst = dut.xcfg.NewXData("CacheCFG_top.reset")
-    # array_2 = dut.xcfg.NewXDataArray("CacheCFG_top.Cache.dataArray.ram.array_2")
-    # print("Array_2 size: ", len(array_2))
-    # print("Array_2: ", array_2[0])
-    # print("Array_2: ", array_2)
+    print("Pins startwith(CacheCFG_top.clock): ", dut.xcfg.GetSignalNames("CacheCFG_top.clock"))
+    clk = dut.GetInternalSignal("CacheCFG_top.clock")
+    rst = dut.GetInternalSignal("CacheCFG_top.reset")
+    array_2 = dut.GetInternalSignal("CacheCFG_top.Cache.dataArray.ram.array_2", is_array=True)
+    print("Array_2 size: ", len(array_2))
+    print("Array_2  [0]: ", array_2[0])
     dut.reset.AsImmWrite()
     rst.AsImmWrite()
-
-
-    print("Inital: reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst))
+    print("Inital: reset from dpi : {}, reset from xcfg: {}".format(dut.reset.value, rst.value))
     dut.reset.value = 1
-    print("set dpi, reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst))
+    print("set dpi, reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst.value))
     dut.Step(1)
-    print("step, reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst))
+    print("step, reset from dpi   : {}, reset from xcfg: {}".format(dut.reset.value, rst.value))
     rst.value = 0
-    print("set rst, reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst))
+    print("set rst, reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst.value))
     dut.Step(1)
-    print("step, reset from dpi: {}, reset from xcfg: {}".format(dut.reset.value, rst))
-
+    print("step, reset from dpi   : {}, reset from xcfg: {}".format(dut.reset.value, rst.value))
     dut.Finish()
