aefc247da767112cb0516d45e3ab790c35e3f56f
yaozhicheng <yaozhicheng@ict.ac.cn>
2025-04-09 16:18:22 +0800

    refine(example): chiselUT add inner-signal access

--- example/chiselUT/src/test/scala/ALUTestCases.scala
+++ example/chiselUT/src/test/scala/ALUTestCases.scala
@@ -10,11 +10,21 @@ class MultiCycleALUTest extends MultiCycleALUTestBase{
     assert(pins.contains("clock"))
   }
 
+  test("test list inner signals") { fixture =>
+    val alu = fixture.alu
+    val pins = alu.listInnerSignals()
+    println("All inner signals: " + pins.mkString(", "))
+    assert(pins.contains("MultiCycleALU_top.reset"))
+  }
+
   test("test Addition") { fixture =>
     val alu = fixture.alu
     val result = alu.add(5, 3)
     println("Addition result: " + result)
+    val aluState = alu.getDut().GetInternalSignal("MultiCycleALU_top.MultiCycleALU.state")
+    println("ALU state: " + aluState.U().intValue)
     assert(result == 8)
+    assert(aluState.U().intValue == 4)
   }
 
   test("test Subtraction") { fixture =>
