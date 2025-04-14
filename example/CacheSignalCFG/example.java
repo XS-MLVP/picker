86eca1679830f536b8d042ee21d2e46f4acde55d
yaozhicheng <yaozhicheng@ict.ac.cn>
2025-04-14 14:53:17 +0800

    new feat(swig): add GetInternalSignalList/GetInternalSignal for java && test pass

--- example/CacheSignalCFG/example.java
+++ example/CacheSignalCFG/example.java
@@ -1,14 +1,18 @@
 package com.ut;
 
+import com.xspcomm.*;
+
 public class example {
 	public static void main(String[] args) {
 	  UT_CacheCFG dut = new UT_CacheCFG();
-	  dut.xcfg.GetSignalNames("").forEach( a -> {
+	  dut.GetInternalSignalList().forEach( a -> {
 		System.out.println("pin: " + a);
 	  });
 	  for(int i = 0; i<10; i++){
 		dut.Step();
 	  }
+	  XData valid = dut.GetInternalSignal("CacheCFG_top.io_in_req_valid");
+	  System.err.println("valid: " + valid.U().intValue());
 	  dut.Finish();
 	  System.out.println("Step UT_CacheCFG 10 times complete");
 	}
